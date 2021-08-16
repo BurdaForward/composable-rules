daaac38bd160aab04867b454c2a771da764b1db5
Dido (Christoph Poelt) <christoph.poelt@valiton.com>
2021-08-16 16:37:38 +0200

    add code, tests and basic readme

---
+++ README.md
@@ -0,0 +1,95 @@
+# RULES
+
+This is a small library mainly intended to make rules around deeplink
+manipulation more maintainable, readable and composable(we don't wanna end up
+with 8 nested `if`s).
+
+## API
+
+**Matchers**
+
+- `not`: negates a matchers.
+- `always`: A matchers that always matches.
+- `all`: Matcher combinator which takes a list of matchers. It is true when all passed matchers are true, false otherwise.
+- `one`: Matcher combinator which takes a list of matchers. It is only true when at least one of the passed matchers is true, false otherwise.
+
+**Combining and enhancing rules**
+- `injectFacts`: Takes a function and a `rule`. The function is passed the `facts` and can return a new transformed version of `facts`(should copy instead of mutate). This is useful for passing, that are specific to one rule only.
+- `transformOutput`: Takes a function and a `rule`. If the rules `matcher` matches, then the function is called with the output of the rule's `action`. This is useful for modying(immtuable!) an action's return value on a higher level.
+- `applyIf`: Checks if the passed matcher matches before running the rule. Takes a `matcher` function and a `rule` and runs only that rule when the matcher matches in addition to the matcher the rule already has. The rule is then run and the `action`'s value is returned.
+- `applyAll`: Takes `rules` and combines a them so that when run all supplied rules will be run in order for those whose matcher returns `true`. It returns the modified value, in our case the modified URL.
+- `applyFirst`: Takes `rules` and combines a them so that when run, only the first supplied rule will be run whose matcher returns `true`. It returns the modified value, in our case the modified URL.
+- `applyChain`: Takes `rules` and combines a them so that when run, only rules will be run as long as their matcher returns `true`. As soon as a rule does not match it it stops. It returns the modified value, in our case the modified URL.
+
+**Running rules**
+- `run`: Takes a `rule`, `facts` and an intial `value` and runs the rule. It returns the modified value, in our case the modified URL. If no rule matches the retuned value is the original input value.
+- `detailedRun`: Like `run` but with a more detailed output and different default value. Takes a `rule`, `facts` and an intial `value` and runs the rule. It returns an object which looks like this: `{ value: <value>, foundMatch: boolean }`. The value will the modified value, in our case the modified URL or `null` when no rule is matched. `foundMatch` is a boolean indicating if any rule matched.
+
+## The nurl library
+
+To simplify url manipulation we use the [`nurl`](https://github.com/codeinthehole/nurl), which is easier
+to use than NodeJS's `url` module and has an immutable URL type.
+
+## How to use
+
+Rules consists of `matcher`s and `action`s. The `action` will only get executed
+when the `matcher` matches.
+
+```js
+const nurl = require('nurl');
+const { all, applyAll, applyFirst, run} = require('./rules');
+
+// MATCHERS
+const isSaturnHost = (facts, url) => url.hostname === 'mediamarkt.de';
+const hasRewriteParam = (facts, url) => url.hasQueryParam('rewrite');
+
+const myRule = {
+  // use `all` to combine functions requiring both to return `true`
+  matcher: all([isSaturnHost, hasRewriteParam]),
+  // if matcher matches, rewrite to saturn.de, `url` will be url object
+  // and contains modifications made so far by previous rules
+  action: (facts, url) => {
+    return url.setHostname('saturn.de') // return new url object
+  }
+}
+```
+
+Then run the rules like this
+```js
+
+// arbitrary data that is passed to the matchers and actions
+const deeplink = nurl.parse('https://someurl.com/iphone8?param=value');
+const facts = {
+  ...,
+  url: deeplink
+};
+
+// combine rules, using `applyAll` will run all rules with passing matchers in order
+// this only creates a new rule and doesn't run it yet
+const combinedRule  = applyAll([myRule, anotherRule]);
+// run the rule on some data
+const manipulatedUrl = run(
+  combinedRule,
+  facts,
+  deeplink // the initial url
+  );
+
+// OR combine rules, checking for the first match and ignoring the rest
+// this only creates a new rule and doesn't run the rule yet
+const combinedRule  = applyFirst([myRule, anotherRule]);
+// run the rule on some data
+const manipulatedUrl = run(
+  combinedRule,
+  facts,
+  deeplink
+  );
+
+
+// extra facts can be injected locally scoped to a rule like this:
+const addSpecificFacts = (facts) => ({ ...facts, specificData: { a: 42 } })
+const combinedRule  = applyFirst([
+  injectFacts(addSpecificFacts, myRule), // this rule will have access to a transformed facts object
+  anotherRule
+]);
+// run the rule on some data
+```
---
+++ src/index.js
@@ -0,0 +1,216 @@
+// curry a function to a specified arity
+const curryToArity = (fn, arity) => {
+  const resolver =
+    (...args) =>
+    (...innerArgs) => {
+      // make sure the function won't return functions endlessly when called with no arguments
+      const newArgs =
+        arity !== 0 && innerArgs.length === 0 ? [undefined] : innerArgs;
+      const allArgs = [...args, ...newArgs];
+      return allArgs.length >= arity ? fn(...allArgs) : resolver(...allArgs);
+    };
+  return resolver();
+};
+
+/* Small library for combining matching functions in a reusable and
+ * readable way
+ * They can be added to rule which can be run on an object of facts,
+ * producing some output.
+ */
+
+/* MATCHERS: Rules need matchers, functions that assert a certain condition
+ * in order to know whether the rule's action should be applied.
+ * Here are some functions to combine matching functions.
+ */
+const always = () => true;
+
+const not = (matcher) => (facts, value) => !matcher(facts, value);
+
+const one = (matchers) => (facts, value) =>
+  matchers.some((check) => check(facts, value));
+
+const all = (matchers) => (facts, value) =>
+  matchers.every((check) => check(facts, value));
+
+/* COMBINING AND ENHANCING RULES
+ * are basically objects with matcher and an action. The action is only applied
+ * when the matcher matches. The actions receive some facts data and a current
+ * value and return new value which is passed on.
+ */
+
+// allows to locally modify the facts object
+// facts should be copied and NOT mutated to prevent side effects and mutability
+// changes will not leak outside of the current rule
+// example:
+//   injectFacts(oldFacts => ({ ...oldFacts, newProp: '42' }), rule)
+const injectFacts = (mapper, rule) => ({
+  type: "injected",
+  mapper,
+  childRule: rule,
+});
+
+// apply a function to the return value of the rule's action, but only if
+// the matcher matches.
+// example:
+//   transformOutput((value) => value + 42, rule)
+const transformOutput = (fn, rule) => ({
+  type: "transformed",
+  transformer: fn,
+  rule,
+});
+
+// combine a list of rules into a new rule where, once executed, all rules with
+// passing matcher are run in order
+const applyIf = (matcher, rule) => ({
+  type: "if",
+  matcher,
+  rule,
+});
+
+// combine a list of rules into a new rule where, once executed, all rules with
+// passing matcher are run in order
+const applyAll = (rules) => ({
+  type: "all",
+  rules,
+});
+
+// combine a list of rules into a new rule where, once executed, rules are run
+// in order until the first matcher matches
+const applyFirst = (rules) => ({
+  type: "first",
+  rules,
+});
+
+// combine a list of rules into a new rule, where rules are executed as long
+// as the previous rule matched
+const applyChain = (rules) => ({
+  type: "chain",
+  rules,
+});
+
+/* RUNNING RULES
+ * Once you have a rule it can be run with the `run()` function
+ *
+ */
+
+// function which processes our recursive function iteratively
+const trampoline =
+  (fn) =>
+  (...args) => {
+    let result = fn(...args);
+    while (typeof result === "function") {
+      result = result();
+    }
+    return result;
+  };
+
+// like run() but keeps track of whether a matcher of a rule passed
+// so we can stop execution early when using "firstMatch" rules
+// for "injected" or "transformed" rules: they count as matched when the contained rule matches
+// for "all" or "first" rules: at least one matcher needs to have matched
+// for simple rules the matcher needs to have matched
+const runHelpRecSafe = (rule, facts, state) => {
+  switch (rule.type) {
+    case "injected":
+      return () => runHelpRecSafe(rule.childRule, rule.mapper(facts), state);
+    case "transformed": {
+      return () => {
+        const { foundMatch, value } = runHelpRecSafe(rule.rule, facts, state);
+        return {
+          foundMatch,
+          value: foundMatch ? rule.transformer(value) : value,
+        };
+      };
+    }
+    case "if": {
+      return () => {
+        const subRule = rule.rule;
+        return rule.matcher(facts, state.value)
+          ? runHelpRecSafe(subRule, facts, state)
+          : { foundMatch: false, value: state.value };
+      };
+    }
+    case "all":
+      // eslint-disable-next-line no-use-before-define
+      return () => runAllMatchingRules(rule.rules, facts, state);
+    case "first":
+      // eslint-disable-next-line no-use-before-define
+      return () => runFirstMatchingRule(rule.rules, facts, state);
+    case "chain":
+      // eslint-disable-next-line no-use-before-define
+      return () => runChainOfRules(rule.rules, facts, state);
+    default:
+      return rule.matcher(facts, state.value)
+        ? { foundMatch: true, value: rule.action(facts, state.value) }
+        : { foundMatch: false, value: state.value };
+  }
+};
+
+const runHelp = trampoline(runHelpRecSafe);
+
+const runAllMatchingRules = (rules, facts, state) => {
+  const reducer = (currentState, currentRule) => {
+    const newState = runHelp(currentRule, facts, currentState);
+    return {
+      foundMatch: currentState.foundMatch || newState.foundMatch,
+      value: newState.value,
+    };
+  };
+  return rules.reduce(reducer, state);
+};
+
+const runFirstMatchingRule = (rules, facts, state) => {
+  const [nextRule, ...remainingRules] = rules;
+  if (!nextRule) {
+    return state;
+  }
+  const { foundMatch, value } = runHelp(nextRule, facts, state);
+  return foundMatch
+    ? { foundMatch, value }
+    : runFirstMatchingRule(remainingRules, facts, state);
+};
+
+const runChainOfRules = (rules, facts, state) => {
+  const [nextRule, ...remainingRules] = rules;
+  if (!nextRule) {
+    return state;
+  }
+  const { foundMatch, value } = runHelp(nextRule, facts, state);
+  return foundMatch
+    ? runChainOfRules(remainingRules, facts, { foundMatch, value })
+    : state;
+};
+
+const run = curryToArity((rule, facts, initialValue) => {
+  const state = { foundMatch: false, value: initialValue };
+  const { value } = runHelp(rule, facts, state);
+  return value;
+}, 3);
+
+// like run but with a more detailed return value
+// Instead of the plain value it returns an object with the the following shape:
+//    { value: <value>, foundMatch: <> }
+//    - value: the actual value built-up by the rules, or `null` if no rule matched
+//        note that unlike `run` this used `null` instead of the orginal value
+//        when no rule matches
+//    - foundMatch: is a boolean which indicates whether any rule in this run matched
+const detailedRun = curryToArity((rule, facts, initialValue) => {
+  const state = { foundMatch: false, value: initialValue };
+  const result = runHelp(rule, facts, state);
+  return result.foundMatch ? result : { value: null, foundMatch: false };
+}, 3);
+
+export {
+  not,
+  one,
+  always,
+  all,
+  injectFacts,
+  transformOutput,
+  applyIf,
+  applyAll,
+  applyFirst,
+  applyChain,
+  run,
+  detailedRun,
+};
